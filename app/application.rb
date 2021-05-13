6a3fd9f0ad55c7f7e2018b9630e37406d8d04c32
pj <pjhg14@gmail.com>
2021-05-13 16:32:29 -0400

    Completed Delete endpoints for post and review

--- app/application.rb
+++ app/application.rb
@@ -30,6 +30,7 @@ class Application
 
       return [200, { 'Content-Type' => 'application/json' }, [ review_to_json ]]
 
+
     elsif req.path.match(/comics/) && req.get?    # Find comic by ID (for review adding)
       id = req.path.split("/").last
       comic = Comic.find(id).to_json({include: 
@@ -46,10 +47,30 @@ class Application
       comic = Comic.find(new_review.comic.id).to_json({include: 
         {reviews: 
           {include: :user}
-        }
+        }, methods: [:rating_average]
       })
       
       return [201, { 'Content-Type' => 'application/json' }, [ comic ]]
+
+    elsif req.path.match("/reviews/") && req.delete?  # Delete review
+      # find review
+      id = req.path.split("/").last
+      review = Review.find(id)
+      
+      # find comic the review used to be part of
+      comic = Comic.find(review.comic.id)
+
+      # Delete review
+      review.destroy()
+
+      # return updated comic with table joins
+      updated_comic = comic.to_json({include: 
+        {reviews: 
+          {include: :user}
+        }, methods: [:rating_average]
+      })
+
+      return [200, { 'Content-Type' => 'application/json' }, [ updated_comic ]]
 
     elsif req.path.match("/groups") && req.get?   # Get all groups
       group_to_json = Group.all.to_json({
@@ -76,7 +97,25 @@ class Application
       
       return [201, { 'Content-Type' => 'application/json' }, [ new_post ]]
 
-    elsif req.path.match("/posts/") && req.get?   # Getting related to a specified group
+    elsif req.path.match("/posts/") && req.delete?    # Delete post
+      # find post
+      id = req.path.split("/").last
+      post = Post.find(id)
+      
+      # find group post used to be part of
+      group = Group.find(post.group.id)
+
+      # Delete post
+      post.destroy()
+
+      # return filtered list of posts
+      filtered_posts = group.posts.to_json({
+        include: :user
+      })
+
+      return [200, { 'Content-Type' => 'application/json' }, [ filtered_posts ]]
+
+    elsif req.path.match("/posts/") && req.get?   # Get posts related to a specified group
       id = req.path.split("/").last
       posts = Group.find(id).posts.to_json({
         include: :user
