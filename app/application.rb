90a3235a67fc249646baef29f15b61f75e6133f6
pjhg14 <pjhg14@gmail.com>
2021-09-29 16:03:05 -0400

    Modified routes

--- app/application.rb
+++ app/application.rb
@@ -4,22 +4,39 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/login/) && req.get?    #Login
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+    # User Paths //////////////////////////////////////////////////////////////////////////////////////////////// #
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+    # Login
+    if req.path == "/users" && req.get?
+      users = User.all
+
+      return [200, { 'Content-Type' => 'application/json' }, [ users.to_json ]]
+
+    elsif req.path.match(/login/) && req.get?
       email = req.path.split("/").last
       user = User.find_by_email(email)
 
       if !!user
         return [200, { 'Content-Type' => 'application/json' }, [ user.to_json ]]
       else
-        return [400, { 'Content-Type' => 'application/json' }, [ {}.to_json ]]
+        return [404, { 'Content-Type' => 'application/json' }, [ {}.to_json ]]
       end
 
-    elsif req.path.match("/sign_up") && req.post?   #Sign up
+    # Sign up
+    elsif req.path.match("/sign_up") && req.post?
+      users = User.all
       hash = JSON.parse(req.body.read)
       new_user = User.create(hash).to_json
-      
-      return [201, { 'Content-Type' => 'application/json' }, [ new_user ]]
 
+      return [201, { 'Content-Type' => 'application/json' }, [ new_user ]]
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+    # Comics Path /////////////////////////////////////////////////////////////////////////////////////////////// #
+    # /////////////////////////////////////////////////////////////////////////////////////////////////////////// #
     elsif req.path == "/comics" && req.get?   # Get all comics
       comic_to_json = Comic.all.to_json({include: 
         {reviews: 
@@ -29,13 +46,15 @@ class Application
 
       return [200, { 'Content-Type' => 'application/json' }, [ comic_to_json ]]
     
-    elsif req.path == "/reviews" && req.get?    # get all reviews
+    
+    # Get all Reviews
+    elsif req.path == "/reviews" && req.get?
       review_to_json = Review.all.to_json
 
       return [200, { 'Content-Type' => 'application/json' }, [ review_to_json ]]
 
-
-    elsif req.path.match(/comics/) && req.get?    # Find comic by ID (for review adding)
+    # Get Comic by ID
+    elsif req.path.match(/comics/) && req.get?
       id = req.path.split("/").last
       comic = Comic.find(id).to_json({include: 
         {reviews: 
@@ -45,7 +64,8 @@ class Application
       
       return [200, { 'Content-Type' => 'application/json' }, [ comic ]]
 
-    elsif req.path.match("/reviews") && req.post?   # Post new review
+    # Post new Review
+    elsif req.path.match("/reviews") && req.post?
       hash = JSON.parse(req.body.read)
       new_review = Review.create(hash)
       comic = Comic.find(new_review.comic.id).to_json({include: 
@@ -56,7 +76,8 @@ class Application
       
       return [201, { 'Content-Type' => 'application/json' }, [ comic ]]
 
-    elsif req.path.match("/reviews/") && req.delete?  # Delete review
+    # Delete Review
+    elsif req.path.match("/reviews/") && req.delete?
       # find review
       id = req.path.split("/").last
       review = Review.find(id)
@@ -76,6 +97,7 @@ class Application
 
       return [200, { 'Content-Type' => 'application/json' }, [ updated_comic ]]
 
+    
     elsif req.path.match("/groups") && req.get?   # Get all groups
       group_to_json = Group.all.to_json({
         include: {
